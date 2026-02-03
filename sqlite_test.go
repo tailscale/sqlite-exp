71772105ddc1962240d78e8b2f6b818fcb95daac
Anton Tolchanov <anton@tailscale.com>
2026-02-03 18:04:46 +0000

    go.mod: rename to github.com/tailscale/sqlite-exp
    Updates tailscale/corp#36170
    
    Signed-off-by: Anton Tolchanov <anton@tailscale.com>

--- sqlite_test.go
+++ sqlite_test.go
@@ -22,7 +22,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/tailscale/sqlite/sqliteh"
+	"github.com/tailscale/sqlite-exp/sqliteh"
 )
 
 func TestOpenDB(t *testing.T) {
@@ -1300,7 +1300,7 @@ func BenchmarkBeginTxNoop(b *testing.B) {
 // This tests that we don't give the same *stmt to two different callers that
 // prepare the same persistent query. See:
 //
-//	https://github.com/tailscale/sqlite/issues/73
+//	https://github.com/tailscale/sqlite-exp/issues/73
 func TestPrepareReuse(t *testing.T) {
 	db := openTestDB(t)
 	ctx := context.Background()
